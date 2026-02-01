a242ab074b519b2f99aa6dd47cddc58e790484b7
MIlAN-JS <milantamang350359@gmail.com>
2026-02-01 08:56:26 +0545

    feat : footer navigation created in project-by-sheryians

---
+++ project-by-sheryians/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./app.scss"
+import Dock from './components/dock/Dock.jsx'
+
+
+const App = () => {
+  return (
+    <main className='main'>
+
+      <Dock/>
+
+    </main>
+  )
+}
+
+export default App
---
+++ project-by-sheryians/src/components/dock/Dock.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import "./dock.scss"
+
+const Dock = () => {
+  return (
+    <footer className='footer'>
+        <i className="icon calender">
+            <img src="../../../public/ankurdotio mac-os main public-doc-icons/calender.svg" alt="" />
+        </i>
+        <i className="icon cli">
+            <img src="../../../public/ankurdotio mac-os main public-doc-icons/cli.svg" alt="" />
+        </i>
+        <i className="icon github">
+            <img src="../../../public/ankurdotio mac-os main public-doc-icons/github.svg" alt="" />
+        </i>
+        <i className="icon link">
+            <img src="../../../public/ankurdotio mac-os main public-doc-icons/link.svg" alt="" />
+        </i>
+        <i className="icon mail">
+            <img src="../../../public/ankurdotio mac-os main public-doc-icons/mail.svg" alt="" />
+        </i>
+        <i className="icon note">
+            <img src="../../../public/ankurdotio mac-os main public-doc-icons/note.svg" alt="" />
+        </i>
+        <i className="icon pdf">
+            <img src="../../../public/ankurdotio mac-os main public-doc-icons/pdf.svg" alt="" />
+        </i>
+        <i className="icon spotify">
+            <img src="../../../public/ankurdotio mac-os main public-doc-icons/spotify.svg" alt="" />
+        </i>
+       
+
+    </footer>
+  )
+}
+
+export default Dock
